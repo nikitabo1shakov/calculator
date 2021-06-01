75d3e77b6a3107cd5af831faad7d43da1c37baec
nikita_bolshakov <nikitas.bolshakov@gmail.com>
2021-06-01 09:33:35 +0300

    homework for lesson 3

---
+++ app/src/main/java/com/nikitabolshakov/calculator/Calculator.java
@@ -0,0 +1,90 @@
+package com.nikitabolshakov.calculator;
+
+public class Calculator {
+
+    private final String BUTTON_0 = "0";
+    private final String BUTTON_1 = "1";
+    private final String BUTTON_2 = "2";
+    private final String BUTTON_3 = "3";
+    private final String BUTTON_4 = "4";
+    private final String BUTTON_5 = "5";
+    private final String BUTTON_6 = "6";
+    private final String BUTTON_7 = "7";
+    private final String BUTTON_8 = "8";
+    private final String BUTTON_9 = "9";
+    private final String BUTTON_DOT = ".";
+    private final String BUTTON_CANCEL = "C";
+    private final String BUTTON_ADDITION = "+";
+    private final String BUTTON_DIVISION = "/";
+    private final String BUTTON_SUBTRACTION = "-";
+    private final String BUTTON_MULTIPLICATION = "×";
+    private final String BUTTON_EQUALLY = "=";
+
+    public String getBUTTON_0() {
+        return BUTTON_0;
+    }
+
+    public String getBUTTON_1() {
+        return BUTTON_1;
+    }
+
+    public String getBUTTON_2() {
+        return BUTTON_2;
+    }
+
+    public String getBUTTON_3() {
+        return BUTTON_3;
+    }
+
+    public String getBUTTON_4() {
+        return BUTTON_4;
+    }
+
+    public String getBUTTON_5() {
+        return BUTTON_5;
+    }
+
+    public String getBUTTON_6() {
+        return BUTTON_6;
+    }
+
+    public String getBUTTON_7() {
+        return BUTTON_7;
+    }
+
+    public String getBUTTON_8() {
+        return BUTTON_8;
+    }
+
+    public String getBUTTON_9() {
+        return BUTTON_9;
+    }
+
+    public String getBUTTON_DOT() {
+        return BUTTON_DOT;
+    }
+
+    public String getBUTTON_CANCEL() {
+        return BUTTON_CANCEL;
+    }
+
+    public String getBUTTON_ADDITION() {
+        return BUTTON_ADDITION;
+    }
+
+    public String getBUTTON_DIVISION() {
+        return BUTTON_DIVISION;
+    }
+
+    public String getBUTTON_SUBTRACTION() {
+        return BUTTON_SUBTRACTION;
+    }
+
+    public String getBUTTON_MULTIPLICATION() {
+        return BUTTON_MULTIPLICATION;
+    }
+
+    public String getBUTTON_EQUALLY() {
+        return BUTTON_EQUALLY;
+    }
+}
